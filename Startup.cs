027881697f45fefce58849353e868b81960817ca
Per Ivar Nerseth <perivar@nerseth.com>
2020-09-14 20:26:19 +0200

    add swagger ui

--- Startup.cs
+++ Startup.cs
@@ -67,11 +67,24 @@ namespace CAMToolsNet
 			});
 
 			services.AddControllers();
+
+			services.AddSwaggerGen();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			// Enable middleware to serve generated Swagger as a JSON endpoint.
+			app.UseSwagger();
+
+			// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+			// specifying the Swagger JSON endpoint.
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+				c.RoutePrefix = string.Empty;
+			});
+
 			app.UseForwardedHeaders();
 
 			if (env.IsDevelopment())
@@ -79,7 +92,7 @@ namespace CAMToolsNet
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.UseHttpsRedirection();
+			// app.UseHttpsRedirection(); // disable to use within docker behind a proxy
 
 			app.UseRouting();
 
