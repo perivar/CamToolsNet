082a625a22e7a82e7dceb49cdaec919ef1a9837e
Per Ivar Nerseth <perivar@nerseth.com>
2021-01-24 00:25:15 +0100

    fixed erros in package.json and changed test

--- camclient/src/App.test.tsx
+++ camclient/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders save dxf link', () => {
+test('renders save svg link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/DXF Circles to Layers/i);
+  const linkElement = getByText(/Save SVG/i);
   expect(linkElement).toBeInTheDocument();
 });
